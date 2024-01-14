e1e65ee5b05c9092ad604c346b75f8626be79a63
bru1f0rc3 <wint1841@inbox.ru>
2024-01-14 10:08:43 +0300

    .

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double E = 0.001;
+            double A, S;
+            int N;
+            Console.Write("Введите X:");
+            double x = double.Parse(Console.ReadLine());
+            S = 0;
+            N = 0;
+            A = x / 2;
+            do
+            {
+                A = A * 1 / ((2 * N + 2) * (2 * N + 1));
+                S = S + A;
+                N += 1;
+            }
+            while (A >= E);
+            Console.WriteLine($"Результат: {S:f2} Число итераций: {N}\n");
+        }
+    }
+}
